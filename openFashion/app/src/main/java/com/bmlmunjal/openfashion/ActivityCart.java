37d7ce974572b0c360d80be086285533f375573b
Manikanta <mandala.manikanta.20cse@bmu.edu.in>
2022-09-26 00:23:48 +0530

    frontEnd card view added for cart

--- openFashion/app/src/main/java/com/bmlmunjal/openfashion/ActivityCart.java
+++ openFashion/app/src/main/java/com/bmlmunjal/openfashion/ActivityCart.java
@@ -1,14 +1,21 @@
 package com.bmlmunjal.openfashion;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class ActivityCart extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        recyclerView = findViewById(R.id.cartCardRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
